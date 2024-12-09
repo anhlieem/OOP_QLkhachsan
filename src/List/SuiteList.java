d64db670e8efa774650447db4d4a50ec0b6d05fd
hau <3123410101@sv.sgu.edu.vn>
2024-12-09 17:07:06 +0700

    update nua neeeeeeeee

---
+++ src/List/SuiteList.java
@@ -0,0 +1,30 @@
+package List;
+import AbstractCore.TypeList;
+public class SuiteList implements TypeList {
+    
+    @Override
+    public void add() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void search() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void display() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
