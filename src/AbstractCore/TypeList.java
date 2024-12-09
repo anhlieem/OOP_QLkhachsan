5cbfdeb130d295a599f2a2e88227ed75736b5dd4
hau <3123410101@sv.sgu.edu.vn>
2024-12-09 16:37:46 +0700

    them typelist

---
+++ src/AbstractCore/TypeList.java
@@ -0,0 +1,9 @@
+package AbstractCore;
+
+public abstract class TypeList {
+    public abstract void add();
+    public abstract void remove();
+    public abstract void update();
+    public abstract void search();
+    public abstract void display();
+}
